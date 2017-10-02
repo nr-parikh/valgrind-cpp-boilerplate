4d65695b200635329097b6bd4f0c6dcaac239c26
Neel Parikh <nrparikh@umd.edu>
2017-10-02 00:10:13 -0400

    Errors solved.

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,6 +1,7 @@
 #include <AnalogSensor.hpp>
 #include <numeric>
 #include <vector>
+#include <memory>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
     : mSamples(samples)
@@ -13,8 +14,9 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+    //std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
+    std::unique_ptr<std::vector <int> > readings = std::make_unique<std::vector<int> > (mSamples, 10);
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
 }
